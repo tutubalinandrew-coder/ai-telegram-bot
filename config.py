82025a5237cd782a626289d51d8cc531d9ca630d
Andrew <tutubalinandrew@gmail.com>
2026-05-12 13:57:02 +0300

    Refactor config management

--- config.py
+++ config.py
@@ -2,18 +2,23 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-if not BOT_TOKEN:
-    raise ValueError('Ошибка - бот_токен отсутствует')
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
-if not OPENAI_API_KEY:
-    raise ValueError('Ошибка - OPENAI_API_KEY отсутствует')
+
+def get_env(name):
+    env = os.environ.get(name)
+    if not env:
+         raise ValueError(f'Ошибка - {name} отсутствует')
+    return env
+
+BOT_TOKEN = get_env("BOT_TOKEN")
+
+OPENAI_API_KEY = get_env("OPENAI_API_KEY")
+
 SYSTEM_PROMPT = "Ты - помощник, который помогает пользователю с его вопросами."
 MAX_HISTORY = 20
 ERROR_MESSAGE = "Не получилось получить ответ от AI. Попробуйте ещё раз чуть позже."
-OPENAI_MODEL = "gpt-4o-mini"
-POSTGRES_DB = os.environ.get("POSTGRES_DB", "")
-POSTGRES_USER = os.environ.get("POSTGRES_USER", "")
-POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "")
-POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "")
-POSTGRES_PORT = os.environ.get("POSTGRES_PORT", "")
+OPENAI_MODEL = get_env("OPENAI_MODEL")
+POSTGRES_DB = get_env("POSTGRES_DB")
+POSTGRES_USER = get_env("POSTGRES_USER")
+POSTGRES_PASSWORD = get_env("POSTGRES_PASSWORD")
+POSTGRES_HOST = get_env("POSTGRES_HOST")
+POSTGRES_PORT = get_env("POSTGRES_PORT")
