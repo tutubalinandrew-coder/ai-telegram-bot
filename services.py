00db7cf886d6b10a13f6f39e984de32ccadaad27
Andrew <tutubalinandrew@gmail.com>
2026-05-16 23:39:49 +0300

    Add conversation memory

--- services.py
+++ services.py
@@ -1,13 +1,20 @@
-from repository import save_message
+from repository import get_last_messages, save_message
 from ai import get_ai_response
 from repository import get_or_create_user
 
 
 async def process_chat(message, db):
     user = get_or_create_user(message.user_id, db)
-    messages = [
-        {"role": "user", "content": message.text}
-    ]
+    history = get_last_messages(message.user_id, 20, db)
+    history.reverse()
+    messages = []
+
+    for role, text in history:
+        messages.append(
+        {"role": role, "content": text}
+        )  
+    messages.append({"role": "user", "content": message.text})
+    
     save_message(message.user_id, "user", message.text, db)
     answer = await get_ai_response(messages)
     save_message(message.user_id, "assistant", answer, db)
